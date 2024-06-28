c23990dab9bdd1a6841e3490ce85c4e4623e0a03
jrjthompson <jrjt.thompson@gmail.com>
2024-06-28 09:51:21 -0700

    updating alc function for any number of dimensions

--- R/alc.R
+++ R/alc.R
@@ -1,8 +1,5 @@
-set.seed(42+seed)## Generate data
-data <- data.generate(n,data.type = data.type,sigma = sigma)
-
-alc <- function(X,Y,data.type="uniformJump",sigma,bw.fixed.value=NULL,
-                            repeats=1,ckertype,bwmethod,regtype){
+alc <- function(X,Y,bw.fixed.value=NULL,
+                resmooths=1,ckertype="gaussian",bwmethod="cv.ls",regtype="lc"){
   ## Fit data using local constant kernel estimator (LC)
   bw.lc <- npregbw(Y~X,regtype=regtype,bwmethod=bwmethod,ckertype=ckertype)
   model.lc <- npreg(bw.lc)
@@ -10,8 +7,8 @@ alc <- function(X,Y,data.type="uniformJump",sigma,bw.fixed.value=NULL,
 
   ## Use LC \widehat{g}(x) as pilot g(x) in ALC for the first time,
   ## and then ALC for each repeat
-  for (i in 1:repeats){
-    data <- data.frame(oracle=data$oracle,X=data$X,Y=data$Y,gX=data$oracle) #replace data$oracle with model.alc$mean if you want ALC smoothing
+  for (i in 1:resmooths){
+    gX <- model.alc$mean
     ## Refit data using the local constant estimator as an input
     bw.llc <- npregbw(Y~X+gX,regtype=regtype,bwmethod=bwmethod,ckertype=ckertype)
     if (!is.null(bw.fixed.value)){
@@ -22,7 +19,3 @@ alc <- function(X,Y,data.type="uniformJump",sigma,bw.fixed.value=NULL,
   model.alc
 }
 
-## Check fit with oracle function and retern mean squared error
-model.lc.ESE <- 0 #we already have this so we don't bother
-model.alc.ESE <- mean((model.alc$mean-data$oracle)^2)
-c(model.lc.ESE,model.alc.ESE)
