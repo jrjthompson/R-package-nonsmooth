76ef1bd46aaa82f31cfda843f76e2c97c191948e
John Thompson <jrjt.thompson@gmail.com>
2024-06-30 13:34:39 -0700

    updating examples and alc function to work correctly together

--- R/alc.R
+++ R/alc.R
@@ -1,17 +1,19 @@
 alc <- function(X,Y,bw.fixed.value=NULL,resmooths=1,...){
   ## Fit data using local constant kernel estimator (LC)
-  bw.lc <- npregbw(Y~X,regtype=regtype,bwmethod=bwmethod,ckertype=ckertype)
+  bw.lc <- npregbw(xdat=X,ydat=Y,...)
   model.lc <- npreg(bw.lc)
   model.alc <- model.lc #set this temporarily
 
   ## Use LC \widehat{g}(x) as pilot g(x) in ALC for the first time,
   ## and then ALC for each repeat
   for (i in 1:resmooths){
-    gX <- model.alc$mean
+    X.alc <- data.frame(X,gx=model.alc$mean)
     ## Refit data using the local constant estimator as an input
-    bw.llc <- npregbw(Y~X+gX,...)
     if (!is.null(bw.fixed.value)){
-      bw.llc$bw <- c(bw.llc$bw[1:dim(X)[2]],bw.fixed.value)
+      bw.llc <- npregbw(xdat=X.alc,ydat=Y,bandwidth.compute=FALSE,
+                        bws=c(bw.lc$bw[1:(dim(X)[2])],bw.fixed.value),...)
+    } else {
+      bw.llc <- npregbw(xdat=X.alc,ydat=Y,...)
     }
     model.alc <- npreg(bw.llc)
   }
